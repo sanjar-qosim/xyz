aae3f7a66601fb743ea3925793d3771b210873b1
Пользователь <sanjar171766@gmail.com>
2022-12-30 11:38:59 +0500

    second case

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,28 +13,43 @@ public class Main {
 //        For home PC
 //        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Home\\chromedriver_win32\\chromedriver.exe");
 
+//        For laptop
+        System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver.exe");
+
 //        For work PC
-        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+//        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
-        Thread.sleep(5000);
+        Thread.sleep(4000);
 
         System.out.println("Start first case");
         driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div[1]/button")).click();
-        Thread.sleep(5000);
+        Thread.sleep(4000);
         driver.findElement(By.xpath("/html/body/div/div/div[2]/div/form/div/select")).click();
         driver.findElement(By.xpath("/html/body/div/div/div[2]/div/form/div/select/option[2]")).click();
         driver.findElement(By.xpath("/html/body/div/div/div[1]/strong")).click();
         driver.findElement(By.xpath("/html/body/div/div/div[2]/div/form/button")).click();
-        Thread.sleep(5000);
+        Thread.sleep(4000);
         driver.findElement(By.xpath("/html/body/div/div/div[1]/button[2]")).click();
-        Thread.sleep(5000);
+        Thread.sleep(4000);
         driver.findElement(By.xpath("/html/body/div/div/div[1]/button[1]")).click();
-        Thread.sleep(5000);
+        Thread.sleep(4000);
         System.out.println("First case was successfully\n");
 
         System.out.println("Start second case");
+        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div[2]/button")).click();
+        Thread.sleep(4000);
+        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/button[1]")).click();
+        Thread.sleep(4000);
+        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[1]/input")).sendKeys("Sanjar");
+        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[2]/input")).sendKeys("Qosimov");
+        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[3]/input")).sendKeys("70007");
+        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/button")).click();
+        driver.switchTo().alert().accept();
+        driver.findElement(By.xpath("/html/body/div/div/div[1]/button[1]")).click();
+        System.out.println("Second case was successfully");
 
 
 
