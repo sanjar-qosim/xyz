73049996fa61f67c2af46884603287c93c6cacf7
sanjar-qosim <sanjar171766@gmail.com>
2023-01-03 22:50:21 +0500

    optmization

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,50 +18,49 @@ public class Main {
 
 //        For work PC
 //        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
-
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
         Thread.sleep(4000);
 
         System.out.println("Start first case");
-        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div[1]/button")).click();
-        Thread.sleep(4000);
-        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/form/div/select")).click();
-        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/form/div/select/option[2]")).click();
-        driver.findElement(By.xpath("/html/body/div/div/div[1]/strong")).click();
-        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/form/button")).click();
-        Thread.sleep(4000);
-        driver.findElement(By.xpath("/html/body/div/div/div[1]/button[2]")).click();
-        Thread.sleep(4000);
-        driver.findElement(By.xpath("/html/body/div/div/div[1]/button[1]")).click();
-        Thread.sleep(4000);
-        System.out.println("First case was successfully\n");
+        driver.findElement(By.cssSelector("[ng-click='customer()']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.id("userSelect")).click();
+        driver.findElement(By.cssSelector("[value='1']")).click();
+        driver.findElement(By.cssSelector("[type='submit']")).submit();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("[ng-click='deposit()']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("[ng-model='amount']")).sendKeys("555");
+        driver.findElement(By.cssSelector("[type='submit']")).submit();
+        driver.findElement(By.cssSelector("[ng-click='home()']")).click();
+        System.out.println("Finish first case\n\n");
 
         System.out.println("Start second case");
-        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div[2]/button")).click();
-        Thread.sleep(4000);
-        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/button[1]")).click();
-        Thread.sleep(4000);
-        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[1]/input")).sendKeys("Sanjar");
-        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[2]/input")).sendKeys("Qosimov");
-        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[3]/input")).sendKeys("70007");
-        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/button")).click();
+        driver.findElement(By.cssSelector("[ng-click='manager()']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("[ng-click='addCust()']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("[ng-model='fName']")).sendKeys("Sanjar");
+        driver.findElement(By.cssSelector("[ng-model='lName']")).sendKeys("Qosimov");
+        driver.findElement(By.cssSelector("[ng-model='postCd']")).sendKeys("7007");
+        driver.findElement(By.cssSelector("[type='submit']")).submit();
+        Thread.sleep(2000);
         driver.switchTo().alert().accept();
-        driver.findElement(By.xpath("/html/body/div/div/div[1]/button[1]")).click();
-        System.out.println("Second case was successfully\n");
-
+        driver.findElement(By.cssSelector("[ng-click='home()']")).click();
+        System.out.println("Finish second case\n\n");
 
         System.out.println("Start third case");
-        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div[2]/button")).click();
-        Thread.sleep(4000);
-        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/button[3]")).click();
-        Thread.sleep(4000);
-        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/form/div/div/input")).sendKeys("Harry");
+        driver.findElement(By.cssSelector("[ng-click='manager()']")).click();
         Thread.sleep(3000);
-        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/table/tbody/tr/td[5]/button")).click();
-        driver.findElement(By.xpath("/html/body/div/div/div[1]/button[1]")).click();
-        System.out.println("Finish third case");
+        driver.findElement(By.cssSelector("[ng-click='showCust()']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("[ng-model='searchCustomer']")).sendKeys("Ron");
+        driver.findElement(By.cssSelector("[ng-click='deleteCust(cust)']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("[ng-click='home()']")).click();
+        System.out.println("Finish third case\n\n");
 
         driver.close();
 
